16733f471b7189732e9a21f12760e584a8fa03e9
Enzo Piriou <enzopiriou@gmail.com>
2022-05-20 04:58:07 +0200

    handle different method

--- api/twilight/index.ts
+++ api/twilight/index.ts
@@ -1,17 +1,26 @@
 import { copy, ServerRequest } from "../../deps.ts";
 
 export default async (req: ServerRequest) => {
-  // const file = await Deno.open('data/');
-  const decoder = new TextDecoder("utf-8");
+	console.log("method:", req.method)
+	if (req.method === 'GET') {
 
-  const data = await Deno.readFile("data/twilight.json");
-  // const data = await Deno.readFile("data/twilight.json");
+		const decoder = new TextDecoder("utf-8");
 
-  const decoded = decoder.decode(data);
-  console.log("decoded:", decoded);
-  //
-  const obj = JSON.parse(decoded);
-  console.log("obj:", obj);
-  //
-  req.respond({ body: decoded });
+		const data = await Deno.readFile("data/twilight.json");
+
+		const decoded = decoder.decode(data);
+		console.log("decoded:", decoded);
+
+		const obj = JSON.parse(decoded);
+		console.log("obj:", obj);
+
+		req.respond({ body: decoded });
+	} else if (req.method === 'POST') {
+		console.log('is POST')
+		const res = JSON.stringify({ data: 'is post' })
+		console.log('is POST after stringify:', res)
+		req.respond({ body: res })
+	} else {
+		req.respond({ body: 'neither GET or POST' })
+	}
 };
