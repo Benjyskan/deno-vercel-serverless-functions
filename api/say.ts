0b69e20dc2e89f416daeab2ee10f0774bb093fe5
Enzo Piriou <enzopiriou@gmail.com>
2022-05-20 02:55:34 +0200

    random test

--- api/say.ts
+++ api/say.ts
@@ -1,11 +1,13 @@
-import { ServerRequest, readAll, say } from '../deps.ts'
+import { ServerRequest, readAll } from '../deps.ts'
+// import { say } from '../deps.ts'
 
 export default async (req: ServerRequest) => {
 	try {
 		const body = new TextDecoder().decode(
 			await readAll(req.body)
 		)
-		const cow = say(JSON.parse(body))
+		// const cow = say(JSON.parse(body))
+		const cow = 'tmp'
 		req.respond({ body: cow })
 	} catch (e) {
 		req.respond({
--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { ServerRequest } from 'https://deno.land/std@0.89.0/http/server.ts'
 export { readAll, copy } from 'https://deno.land/std@0.140.0/streams/conversion.ts'
-export { say } from 'https://deno.land/x/cowsay@1.1/mod.ts'
+// export { say } from 'https://deno.land/x/cowsay@1.1/mod.ts'
