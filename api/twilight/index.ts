4facdad824430998dc5d24e2c0ed6f86d36d4ce3
Enzo Piriou <enzopiriou@gmail.com>
2022-05-20 02:30:26 +0200

    add .vercelignore

--- api/twilight/index.ts
+++ api/twilight/index.ts
@@ -3,7 +3,10 @@ import { ServerRequest, copy } from '../../deps.ts'
 export default async (req: ServerRequest) => {
 	// const file = await Deno.open('data/');
 	const decoder = new TextDecoder("utf-8");
+
 	const data = await Deno.readFile("data/twilight.json");
+	// const data = await Deno.readFile("data/twilight.json");
+
 	const decoded = decoder.decode(data);
 	console.log('decoded:', decoded)
 	//
