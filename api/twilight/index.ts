65547ab595ad57929b3c6229ca4e72583b5f7e71
Enzo Piriou <enzopiriou@gmail.com>
2022-05-24 16:46:05 +0200

    add strategy_cards endpoint

--- api/twilight/index.ts
+++ api/twilight/index.ts
@@ -1,26 +1,23 @@
 import { copy, ServerRequest } from "../../deps.ts";
 
 export default async (req: ServerRequest) => {
-	console.log("method:", req.method)
-	if (req.method === 'GET') {
+  console.log("method:", req.method)
+  if (req.method === 'GET') {
+    const decoder = new TextDecoder("utf-8");
+    const data = await Deno.readFile("data/twilight.json");
+    const decoded = decoder.decode(data);
+    console.log("decoded:", decoded);
+    const obj = JSON.parse(decoded);
+    console.log("obj:", obj);
 
-		const decoder = new TextDecoder("utf-8");
+    req.respond({ body: decoded });
+  } else if (req.method === 'POST') {
+    console.log('is POST')
+    const res = JSON.stringify({ data: 'is post' })
+    console.log('is POST after stringify:', res)
 
-		const data = await Deno.readFile("data/twilight.json");
-
-		const decoded = decoder.decode(data);
-		console.log("decoded:", decoded);
-
-		const obj = JSON.parse(decoded);
-		console.log("obj:", obj);
-
-		req.respond({ body: decoded });
-	} else if (req.method === 'POST') {
-		console.log('is POST')
-		const res = JSON.stringify({ data: 'is post' })
-		console.log('is POST after stringify:', res)
-		req.respond({ body: res })
-	} else {
-		req.respond({ body: 'neither GET or POST' })
-	}
+    req.respond({ body: res })
+  } else {
+    req.respond({ body: 'neither GET or POST' })
+  }
 };
