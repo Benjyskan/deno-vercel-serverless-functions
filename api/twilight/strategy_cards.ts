a7ab43a442736cce718f2c2498c8f0bc50d6a05d
Enzo Piriou <enzopiriou@gmail.com>
2022-05-24 17:11:46 +0200

    add try catch

--- api/twilight/strategy_cards.ts
+++ api/twilight/strategy_cards.ts
@@ -3,13 +3,19 @@ import { ServerRequest } from '../../deps.ts'
 
 export default async (req: ServerRequest) => {
   if (req.method === 'Get') {
-    const decoder = new TextDecoder("utf-8");
-    const data = await Deno.readFile("data/strategy_cards.json");
-    const decoded = decoder.decode(data);
-    console.log("decoded:", decoded);
-    const obj = JSON.parse(decoded);
-    console.log("obj:", obj);
+    try {
+      const decoder = new TextDecoder("utf-8");
+      const data = await Deno.readFile("data/strategy_cards.json");
+      const decoded = decoder.decode(data);
+      console.log("decoded:", decoded);
+      const obj = JSON.parse(decoded);
+      console.log("obj:", obj);
+    } catch (e) {
+      req.respond({ body: e })
+      return e
+    } finally {
 
-    req.respond({ body: decoded });
+      req.respond({ body: decoded });
+    }
   }
 }
