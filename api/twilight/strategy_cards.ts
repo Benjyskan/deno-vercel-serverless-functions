1cc7603cf9bcc43ec5cecb671e5a8686f260b65d
Enzo Piriou <enzopiriou@gmail.com>
2022-05-24 17:02:22 +0200

    copy getStrategies to different path

---
+++ api/twilight/strategy_cards.ts
@@ -0,0 +1,15 @@
+
+import { ServerRequest } from '../../deps.ts'
+
+export default async (req: ServerRequest) => {
+  if (req.method === 'Get') {
+    const decoder = new TextDecoder("utf-8");
+    const data = await Deno.readFile("data/strategy_cards.json");
+    const decoded = decoder.decode(data);
+    console.log("decoded:", decoded);
+    const obj = JSON.parse(decoded);
+    console.log("obj:", obj);
+
+    req.respond({ body: decoded });
+  }
+}
